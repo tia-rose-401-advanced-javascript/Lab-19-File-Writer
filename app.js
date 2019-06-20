c0681d775152c3ce20e5d991f4c4d4f5d3a75a20
Tia Rose <shortyshort789@aol.com>
2019-06-20 16:30:18 -0700

    have app.js sending data to server and logger

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const alter = require('./mod/alter');
+
+// require('dotenv').config();
+const Q = require('@nmq/q/client');
+
+const alterFile = (file) => {
+ 
+  alter.readFile(file)
+    .then(data => {
+      alter.writeFile(file, alter.upper(data));
+      Q.publish('files', 'save', {name: 'test'});
+    })
+    .catch(error => Q.publish('file-error', error));
+};
+
+
+let file = process.argv.slice(2).shift();
+alterFile(file);
